805d1e63069a5e279f0fc07ee14fadf9c7cb135c
Emil Badretdinov <hat-patfaceit@mail.ru>
2022-02-16 17:17:43 +0300

    site1

--- server.py
+++ server.py
@@ -37,30 +37,5 @@ def img():
                   </body>
                 </html>"""
 
-
-@app.route('/promotion_image')
-def prom_img():
-    return f'''<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                        <link rel="stylesheet" 
-                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                        crossorigin="anonymous">
-                        <title>Колонизация</title>
-                      </head>
-                      <body>
-                        <h1>Жди нас, Марс!</h1>
-                        <img width="700px" height="700px" src="{url_for('static', filename='img/mars.png')}"
-                    alt="здесь должна была быть картинка, но не нашлась">
-                        <div class="alert alert-primary" role="alert">
-                          А мы тут компонентами Bootstrap балуемся
-                        </div>
-                      </body>
-                    </html>'''
-
-
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
